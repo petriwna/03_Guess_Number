126b2eb2ed8d32b239f7a138667ead83c0986854
petriwna <petriwna43@gmail.com>
2024-01-08 20:12:12 +0200

    Update 2024-01-08T18:12:10.019Z

--- js/bundle.js
+++ js/bundle.js
@@ -1,13 +1,3 @@
-import {
-  addClassToElement,
-  addEventToElement,
-  disableBtn,
-  enableButton,
-  getNodeValue,
-  removeClassFromElement,
-  setNodeInnerText,
-  setNodeValue,
-} from './utils';
 
 export class Game {
   constructor() {
@@ -209,5 +199,68 @@ export class Game {
 
   validationMinAndMax(value) {
     return value > 200 || value <= 0;
+  }
+}
+
+
+export function init() {
+  new Game();
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+export function addEventToElement(id, eventType, callback) {
+  const node = document.getElementById(id);
+  if (node) {
+    node.addEventListener(eventType, callback);
+  }
+}
+
+export function getNodeValue(id) {
+  const node = document.getElementById(id);
+  if (node) {
+    return node.value;
+  }
+  return '';
+}
+
+export function setNodeValue(id, value) {
+  const node = document.getElementById(id);
+  if (node) {
+    return (node.value = value);
+  }
+  return '';
+}
+
+export function setNodeInnerText(id, value = '') {
+  const node = document.getElementById(id);
+  if (node) {
+    node.innerText = value;
+    return true;
+  }
+  return false;
+}
+
+export function enableButton(id) {
+  const btn = document.getElementById(id);
+  btn.disabled = false;
+}
+
+export function disableBtn(id) {
+  const btn = document.getElementById(id);
+  btn.disabled = true;
+}
+
+export function addClassToElement(id, className) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.classList.add(className);
+  }
+}
+
+export function removeClassFromElement(id, className) {
+  const element = document.getElementById(id);
+  if (element) {
+    element.classList.remove(className);
   }
 }
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import { Game } from './Game';
-
-export function init() {
-  new Game();
-}
-
-document.addEventListener('DOMContentLoaded', init);
